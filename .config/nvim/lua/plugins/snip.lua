4885471ebcb6b4c9f5706242f362d1235081d742
LetticeLover <tylerleo1017@outlook.com>
2026-08-04 10:37:42 -0500

    Updated snip.lua neovim config file to fix bug.

--- .config/nvim/lua/plugins/snip.lua
+++ .config/nvim/lua/plugins/snip.lua
@@ -2,13 +2,11 @@ vim.pack.add({
   'https://github.com/L3MON4D3/LuaSnip',
 })
 require('luasnip').setup({})
-vim.fn.jobstart({'make', '-C', vim.pack.get({'LuaSnip'})[1].path, 'install_jsregexp'}, {
-  on_exit = function(job_id, exit_code, event)
-    if exit_code == 0 then
-      --print('Built jsregexp for LuaSnip')
-    else
-      print('Could not build jsregexp for LuaSnip')
-    end
-  end,
-})
+-- Only run make if jsregexp hasn't been built yet
+local luasnip_path = vim.pack.get({'LuaSnip'})[1].path
+local jsregexp_so = luasnip_path .. '/deps/jsregexp/jsregexp.so'
+
+if vim.fn.filereadable(jsregexp_so) == 0 then
+  vim.fn.jobstart({'make', '-C', luasnip_path, 'install_jsregexp'})
+end
 require('luasnip.loaders.from_vscode').lazy_load()

bdbce5e7ea1c8f0b846f7d855f16328307aabf7a
Correlander <correlander@gmail.com>
2026-08-07 23:54:31 -0500

    Stripped excess bootloaders and changed branding

--- archiso-profile/profiledef.sh
+++ archiso-profile/profiledef.sh
@@ -1,25 +1,23 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
-iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
-iso_application="Arch Linux Live/Rescue DVD"
+iso_name="fast-af"
+iso_label="FASTAF_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Fast-AF Linux <https://github.com/Rising-Bread-Labs/Fast-AF/tree/main>"
+iso_application="Fast-AF UEFI Installer ISO"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
-install_dir="arch"
+install_dir="fastaf"
 buildmodes=('iso')
-bootmodes=('bios.syslinux'
-           'uefi.systemd-boot')
+bootmodes=('uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
-  ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
+  ["/usr/local/bin/fastaf-install-tui"]="0:0:755"
+  ["/usr/local/bin/fastaf-install"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
 )

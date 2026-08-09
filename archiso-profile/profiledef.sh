519b4042b66cc1b0c889c743caa3184cb978133d
Correlander <correlander@gmail.com>
2026-08-09 00:45:19 -0500

    Fixed mkinitcpio hooks and profiledef.sh compression flags

--- archiso-profile/profiledef.sh
+++ archiso-profile/profiledef.sh
@@ -11,7 +11,7 @@ buildmodes=('iso')
 bootmodes=('uefi.systemd-boot')
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'zstd' '-Xbcj' 'x86,arm64' '-b' '1M' '-Xdict-size' '1M')
+airootfs_image_tool_options=('-comp' 'zstd' '-b' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"

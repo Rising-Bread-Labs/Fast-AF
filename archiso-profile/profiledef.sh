54b2c56846d1da74e6cff6d89f87bd2b7ad780d1
Correlander <correlander@gmail.com>
2026-08-12 21:09:38 -0500

    doas default configs and file permissions

--- archiso-profile/profiledef.sh
+++ archiso-profile/profiledef.sh
@@ -15,6 +15,7 @@ airootfs_image_tool_options=('-comp' 'zstd' '-b' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/doas.conf"]="0:0:400"
   ["/root"]="0:0:750"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/fastaf-install-tui"]="0:0:755"
